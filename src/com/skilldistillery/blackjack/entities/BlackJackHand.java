eef65775f3c47d8ac9472dc8427f01910b57f97f
DylanBeppler <dylanbeppler@gmail.com>
2023-12-08 14:16:14 -0600

    initial commit

---
+++ src/com/skilldistillery/blackjack/entities/BlackJackHand.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.blackjack.entities;
+
+public class BlackJackHand extends Hand{
+
+}
---
+++ src/com/skilldistillery/blackjack/entities/Hand.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.blackjack.entities;
+
+import java.util.List;
+
+public abstract class Hand {
+private List<Card> cards;
+}
